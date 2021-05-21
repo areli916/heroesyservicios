43d5a364ddc539372e4b3318edbb8cf83ba08d7f
areli916 <201821117@tese.edu.mx>
2021-05-21 05:58:52 -0500

    heroes con servicios

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+  id: number;
+  name: string;
+  edad: number;
+  poder: string;
+  identidad: string;
+  universo: string;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'hulk', edad: 40, poder: 'destruccion', identidad: 'conocida', universo: 'marvel' },
+  { id: 12, name: 'batman', edad: 23, poder: 'vision nocturna', identidad: 'desconocida', universo: 'DC' },
+  { id: 13, name: 'iroman', edad: 50, poder: 'vision laser', identidad: 'conocida', universo: 'marvel' },
+  { id: 14, name: 'windstrom', edad: 33, poder: 'aeroquinesis', identidad: 'desconocida', universo: 'dc'},
+  { id: 15, name: 'capitan america', edad: 23, poder:'super escudo', identidad: 'conocida', universo: 'dc' },
+  { id: 16, name: 'mr increible', edad: 40, poder: 'fuerza', identidad: 'conocida', universo: 'los increibles' },
+  { id: 17, name: 'flash', edad: 30, poder: 'velocidad', identidad: 'conocida', universo: 'dc'},
+  { id: 18, name: 'superman', edad: 40, poder: 'vuelo', identidad: 'conocida', universo: 'dc' }
+
+
+
+
+
+];
